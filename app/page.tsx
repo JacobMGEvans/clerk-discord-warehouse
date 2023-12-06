63bb8a4fc7d28aa213640e93c46ef0f3e96602b1
Jacob MG Evans <jacobmgevans@gmail.com>
2023-12-06 12:59:13 -0600

    Upgrade

--- app/page.tsx
+++ app/page.tsx
@@ -1,76 +1,55 @@
-import { Suspense } from "react";
-import { PrismaClient } from "@prisma/client";
+import Link from "next/link";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
 import {
-  Collapsible,
-  CollapsibleTrigger,
-  CollapsibleContent,
-} from "@/components/ui/collapsible";
+  CardTitle,
+  CardDescription,
+  CardHeader,
+  CardContent,
+  Card,
+} from "@/components/ui/card";
 
-import SearchBar from "./search";
-
-const prisma = new PrismaClient();
-
-export default async function Page({
-  searchParams,
-}: {
-  searchParams: { [key: string]: string | undefined };
-}) {
-  const forumPosts = await prisma.thread.findMany({
-    include: {
-      messages: {
-        include: {
-          images: true,
-        },
-      },
-    },
-    where: {
-      threadPostTitle: {
-        search: searchParams["threadPostTitle"],
-      },
-    },
-  });
-
+export default function Component() {
   return (
-    <main className="container flex flex-col text-center">
-      <header className="text-center mt-10 mb-6 w-full">
-        <h1 className="text-4xl font-bold">Clerk Discord Warehouse</h1>
-        <Suspense fallback={<div className="mt-2">Falling Back</div>}>
-          <SearchBar />
-        </Suspense>
+    <div className="flex flex-col w-full min-h-screen">
+      <header className="flex items-center h-16 px-4 border-b shrink-0 md:px-6">
+        <Link
+          className="flex items-center gap-2 text-lg font-semibold sm:text-base"
+          href="#"
+        >
+          {/* <ClerkIcon className="w-6 h-6" /> */}
+          <span>Discord Threads</span>
+        </Link>
+        <div className="flex items-center w-full gap-4 md:ml-auto md:gap-2 lg:gap-4">
+          <form className="flex-1">
+            <Input
+              className="bg-white dark:bg-gray-950"
+              placeholder="Search threads..."
+            />
+            <Button className="sr-only" type="submit">
+              Submit
+            </Button>
+          </form>
+        </div>
       </header>
-      {forumPosts.map((forumPost) => {
-        const { id, threadPostTitle, author, messages } = forumPost;
-        return (
-          <Collapsible key={id}>
-            <ul className="border border-gray-300 rounded-md p-4 mx-auto">
-              <CollapsibleTrigger className="flex justify-between items-baseulne">
-                <h2 className="text-xl font-bold">Title: {threadPostTitle}</h2>
-                <h3 className="text-lg">AuthorID: {author}</h3>
-              </CollapsibleTrigger>
-
-              <CollapsibleContent className={"grid gap-1.5 text-center mt-4"}>
-                {messages.map((obj, idx) => {
-                  return (
-                    <article
-                      key={obj.id}
-                      className="border border-gray-200 rounded-md p-2"
-                    >
-                      <p className="block mb-1 font-bold">post {idx}</p>
-                      <p className="mb-2">{obj.content}</p>
-                      {obj.images.map(({ url, id }) => (
-                        <div key={id} className="mb-1">
-                          <b className="block mb-1">Image URL</b>
-                          <p>{url}</p>
-                        </div>
-                      ))}
-                    </article>
-                  );
-                })}
-              </CollapsibleContent>
-            </ul>
-          </Collapsible>
-        );
-      })}
-    </main>
+      <main className="flex min-h-[calc(100vh_-_theme(spacing.16))] bg-gray-100/40 flex-1 flex-col gap-4 p-4 md:gap-8 md:p-10 dark:bg-gray-800/40">
+        <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3 max-w-6xl w-full mx-auto">
+          <Card>
+            <CardHeader className="flex flex-row items-center gap-4">
+              {/* <ClerkIcon className="w-8 h-8" /> */}
+              <div className="grid gap-1">
+                <CardTitle>Thread 1</CardTitle>
+                <CardDescription>Thread 1 Description</CardDescription>
+              </div>
+            </CardHeader>
+            <CardContent className="grid gap-2">
+              <Link className="text-sm font-semibold" href="#">
+                View Messages
+              </Link>
+            </CardContent>
+          </Card>
+        </div>
+      </main>
+    </div>
   );
 }
